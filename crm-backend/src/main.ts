a258112de2f3703d5bd706d71bc2623060b206c6
PoojithaPanja <22b01a12c3@svecw.edu.in>
2026-01-28 04:08:27 +0530

    service, controller folders with required files and dto folders got created with implementing user model db schema

--- crm-backend/src/main.ts
+++ crm-backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('CRM Backend API')
+    .setDescription('Enterprise CRM Backend System')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
